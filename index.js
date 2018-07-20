e14a8ec27ffe45770645a3bd21689c0aa3ee4cc6
gadiel <gadiel.santos@grupomalta.com.br>
2018-07-20 21:46:59 +0000

    add no projeto o body parser

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 require('dotenv').config()
 let express = require('express')
+let bodyParser = require('body-parser')
 
 let port = process.env.PORT || 3000
 
 let index = require('./routes/index')
 
 let app = express()
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }))
 
 app.use('/', index)
 
